a976cdcfdaf9b1f1a5aeda2589755f77652d115a
Jay Mistry <mistry.jay15@gmail.com>
2018-10-19 22:47:20 +0530

    updated about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -13,10 +13,10 @@ const About = () => {
 		},
 		p: {
 			fontSize: '18px',
-			marginBottom: '20px'
+			marginBottom: '5px'
 		},
 		d1: {
-			marginTop: '5px'
+			marginTop: '25px'
 		}
 	}
 
@@ -31,7 +31,25 @@ const About = () => {
 			</Typography>
 
 			<Typography style={styles.p}>
-				Explain why Bhagavada Gita.
+
+			Bhagavada Gita is a book which has always captured my attention.
+			The knowledge and pilosophy it preaches is simply great. The name
+			'Bhagavada Gita' translates to 'The Divine Song'. Arjuna, a great
+			warrior, and a greater human, amassed confusion and reluctance
+			after seeing his kinsmen and relatives on the opposite side of the
+			Kurukshetra battlefield. To get Arjuna rid of this confusion and
+			ignorance, Lord Krishna, the Supreme Deity, preached holy
+			knowledge to him, which came to be known as 'Bhagavada Gita'.
+
+			</Typography>
+
+			<Typography style={styles.p}>
+
+			The knowledge that is present in Bhagavada Gita should be made
+			easily available for everyone to read. No one actually owns the
+			content of this book, so as per me, it should be available for
+			free. So I created this website.
+
 			</Typography>
 
 			<Typography style={styles.d1} variant='display1'>
@@ -39,8 +57,17 @@ const About = () => {
 			</Typography>
 
 			<Typography style={styles.p}>
-				Explain about me.
+
+				I am Jay Mistry, a B.Tech Computer Science undergraduate
+				student at IIIT Vadodara. I invest my time in learning,
+				developing, reading, listening to some great music and forming
+				bonds with people. Currently, I am working with technologies
+				like ReactJS and Node.js. I have experience in developing
+				VueJS, Django and PHP apps. To know more about me, you reach
+				out to me on any of my social handles! 
+			
 			</Typography>
+
 		</div>
 	);
 };
